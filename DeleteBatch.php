2b072d2b785d8f2754bb5a67b48d4daf6440bc30
MarcoAurelio <maurelio@tools.wmflabs.org>
2017-11-17 19:17:32 +0100

    Convert DeleteBatch to use extension registration
    Bug: T180818
    Change-Id: Ibca3f2d3bbb4c9b6128a26ed6c95f50c1ea310f0

--- DeleteBatch.php
+++ DeleteBatch.php
@@ -1,43 +1,35 @@
 <?php
 /**
- * DeleteBatch - a special page to delete a batch of pages
- *
- * @file
- * @ingroup Extensions
- * @author Bartek Łapiński <bartek@wikia-inc.com>
- * @version 1.6.0
- * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- * @link https://www.mediawiki.org/wiki/Extension:DeleteBatch Documentation
- */
-
-// Ensure that the script cannot be executed outside of MediaWiki.
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This is an extension to MediaWiki and cannot be run standalone.' );
+* DeleteBatch - a special page to delete a batch of pages
+*
+* Copyright (C) 2008, Bartek Łapiński <bartek@wikia-inc.com>
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA  02110-1301, USA.
+*/
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'DeleteBatch' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['DeleteBatch'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['DeleteBatchAlias'] = __DIR__ . '/DeleteBatch.alias.php';
+	wfWarn(
+		'Deprecated PHP entry point used for the DeleteBatch extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the DeleteBatch extension requires MediaWiki 1.29+' );
 }
-// Extension credits that will show up on Special:version
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'Delete Batch',
-	'version' => '1.6.0',
-	'author' => array(
-		'Bartek Łapiński',
-		'...'
-	),
-	'url' => 'https://www.mediawiki.org/wiki/Extension:DeleteBatch',
-	'descriptionmsg' => 'deletebatch-desc',
-	'license-name' => 'GPL-2.0-or-later'
-);
-
-// New user right, required to use Special:DeleteBatch
-$wgAvailableRights[] = 'deletebatch';
-$wgGroupPermissions['bureaucrat']['deletebatch'] = true;
-
-// Set up the new special page
-$wgMessagesDirs['DeleteBatch'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['DeleteBatchAlias'] = __DIR__ . '/DeleteBatch.alias.php';
-$wgAutoloadClasses['SpecialDeleteBatch'] = __DIR__ . '/DeleteBatch.body.php';
-$wgAutoloadClasses['DeleteBatchForm'] = __DIR__ . '/DeleteBatch.body.php';
-$wgSpecialPages['DeleteBatch'] = 'SpecialDeleteBatch';
-
-// Hooks
-$wgHooks['AdminLinks'][] = 'SpecialDeleteBatch::addToAdminLinks'; // Admin Links extension
